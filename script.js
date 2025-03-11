969f60bdd540bafb873956cebd6ec062f9a8d0a2
sidkidd <sidkidiyoor@gmail.com>
2025-03-11 18:52:10 +0530

    Recaptcha changes

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ function beforesubmit() {
     outputdate.value = formattedDate;
     console.log(outputdate.value);
 }
+function timestamp() { 
+    var response = document.getElementById("g-recaptcha-response");
+     if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(
+            document.getElementsByName("captcha_settings")[0].value
+        );
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+     }
+     }
+      setInterval(timestamp, 500); 
