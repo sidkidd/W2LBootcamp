be4aed9d5ab580b108c7fc74a483b77cde993eb8
sidkidd <sidkidiyoor@gmail.com>
2025-03-11 19:05:42 +0530

    recaptcha validation

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function beforesubmit() {
+let captchaCheck = false;
+function beforesubmit(event) {
+    if(captchaCheck) {
     let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
     console.log("Input Date Value: ",inputdate.value); // convert STRING to DATE (Locale is en_IN)
 
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdate.value = formattedDate;
-    console.log(outputdate.value);
+
+    } else {
+        alert("Please check the RE-CAPTCHA ");
+        event.preventDefault();
+    }
+    
 }
 function timestamp() { 
     var response = document.getElementById("g-recaptcha-response");
@@ -18,3 +25,7 @@ function timestamp() {
      }
      }
       setInterval(timestamp, 500); 
+
+function captchasuccess() {
+    captchaCheck = true;
+}
